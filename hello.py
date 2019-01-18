91d908cc288d0215e04e1181944fc759c2eb64c3
brandong1 <brandon@brandonleeproductions.com>
2019-01-17 21:45:04 -0700

    added string syntax to hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,43 @@
 #!/usr/bin/env python
 
 print("Hello World") #printing hello
+
+... This is a
+... multi-line string
+... """
+'\nThis is a\nmulti-line string\n'
+>>> "pass"+"word"
+'password'
+>>> "Ha"*4
+'HaHaHaHa'
+>>> "double".find('s')
+-1
+>>> "double".find('d')
+0
+>>> "double".find('o')
+1
+>>> "double".find('bl')
+3
+>>> "TEstINg".lower()
+'testing'
+>>> print("Tab\tDelimited")
+Tab	Delimited
+>>> print("New\nLine")
+New
+Line
+>>> print("Slash\\Character")
+Slash\Character
+>>> print("'Single' are fine")
+'Single' are fine
+>>> print('"Double" are fine')
+"Double" are fine
+>>> print("Your's is awesome")
+Your's is awesome
+>>> print('Your\'s is awesome'
+... print('Your\'s is awesome')
+  File "<stdin>", line 2
+    print('Your\'s is awesome')
+        ^
+SyntaxError: invalid syntax
+>>> print('Your\'s is awesome')
+Your's is awesome
